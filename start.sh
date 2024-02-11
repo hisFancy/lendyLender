52d7c43bda320346e2243738b326685f0384ed2a
hisFancy <hansaroni@ymail.com>
2024-02-11 13:34:37 +0300

    updating start.sh to use voila

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Convert Jupyter notebook to HTML
-jupyter nbconvert --to html --ExecutePreprocessor.timeout=-1 LendyLender.ipynb
+# jupyter nbconvert --to html --ExecutePreprocessor.timeout=-1 LendyLender.ipynb
+voila --no-browser --enable_nbextensions=True LendyLender.ipynb
+
 
 # Start a simple web server to serve the HTML file
-python -m http.server $PORT
+# python -m http.server $PORT
