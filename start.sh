c36a4afc85535910781e24668b56c9e539623958
hisFancy <hansaroni@ymail.com>
2024-02-12 17:37:37 +0300

    removing --enable_nbextensions=True from start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 # Convert Jupyter notebook to HTML
 # jupyter nbconvert --to html --ExecutePreprocessor.timeout=-1 LendyLender.ipynb
-voila --no-browser --enable_nbextensions=True LendyLender.ipynb
+# voila --no-browser --enable_nbextensions=True LendyLender.ipynb
+voila --no-browser LendyLender.ipynb
 
 
 # Start a simple web server to serve the HTML file
